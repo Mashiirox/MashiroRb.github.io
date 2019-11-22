8200fee2cfdb3359d17fa75c62f6bfafd0689031
MashiroRb <alwaysbeenhere@163.com>
2019-11-22 22:31:32 +0800

    Site updated: 2019-11-22 22:31:31

--- js/botui.js
+++ js/botui.js
@@ -22,96 +22,72 @@ function bot_ui_ini() {
     var botui = new BotUI("hello-mashiro")
     botui.message.add({
         delay: 800,
-        content: "Hi, there👋"
+        content: "Hello hello(oﾟ▽ﾟ)o"
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "这里是 Mashiro"
+            content: "你找到我啦|●´∀`|σ"
         }).then(function () {
-            botui.message.add({
-                delay: 1100,
-                content: "一个可爱的蓝孩子~"
-            }).then(function () {
                 botui.action.button({
                     delay: 1600,
                     action: [{
-                        text: "然后呢？ 😃",
+                        text: "嗯！(/≧▽≦)/",
                         value: "sure"
-                    }, {
-                        text: "少废话！ 🙄",
-                        value: "skip"
                     }]
                 }).then(function (a) {
                     "sure" == a.value && sure();
-                    "skip" == a.value && end()
                 })
             })
-        })
-    });
+        });
     var sure = function () {
             botui.message.add({
                 delay: 600,
-                content: "😘"
+                content: "😇"
             }).then(function () {
                 secondpart()
             })
         },
-        end = function () {
-            botui.message.add({
-                delay: 600,
-                content: "![...](https://view.moezx.cc/images/2018/05/06/a1c4cd0452528b572af37952489372b6.md.jpg)"
-            })
-        },
+        
         secondpart = function () {
             botui.message.add({
                 delay: 1500,
-                content: "目前就读于上海财经大学"
+                content: "目前就读于武汉理工大学"
             }).then(function () {
                 botui.message.add({
                     delay: 1500,
-                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
+                    content: "自由随性洒脱的伪Coder"
                 }).then(function () {
                     botui.message.add({
                         delay: 1200,
-                        content: "因为数据分析也需要Coder嘛"
+                        content: "主攻 Python，略懂 Java，精通 C/C++/C#/JavaScript/PHP"
                     }).then(function () {
                         botui.message.add({
                             delay: 1500,
-                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
+                            content: "的 \"Hello World\" 输出方式 "
                         }).then(function () {
-                            botui.message.add({
-                                delay: 1500,
-                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
-                            }).then(function () {
-                                botui.message.add({
-                                    delay: 1800,
-                                    content: "喜欢画画，希望有一天能够被称为画师"
-                                }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            text: "为什么站名是Mashiro呢？(ﾟ▽ﾟ)/",
                                             value: "why-mashiro"
                                         }]
                                     }).then(function (a) {
                                         thirdpart()
                                     })
                                 })
-                            })
                         })
                     })
                 })
-            })
-        },
+            },
         thirdpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
+                content: "因为！椎名真白天下第一！我永远喜欢Shiina Mashiro.jpg"
             }).then(function () {
                 botui.action.button({
                     delay: 1500,
                     action: [{
-                        text: "为什么是白猫呢？ 🤔",
+                        text: "为什么建立这个博客呢？(*≧∪≦)",
                         value: "why-cat"
                     }]
                 }).then(function (a) {
@@ -122,33 +98,33 @@ function bot_ui_ini() {
         fourthpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "因为对GitHub有种执念… "
+                content: "因为一次偶然机会看到 樱花庄的白猫(本站主题作者)的博客 "
             }).then(function () {
                 botui.message.add({
                     delay: 1100,
-                    content: "而且我真的是猫控！"
+                    content: "就想建个博客记录自己写bug的一系列过程(*ﾟДﾟ*) "
                 }).then(function () {
-                    botui.action.button({
-                        delay: 1500,
-                        action: [{
-                            text: "域名有什么含意吗？(ง •_•)ง",
-                            value: "why-domain"
-                        }]
-                    }).then(function (a) {
-                        fifthpart()
+                    botui.message.add({
+                        delay: 1100,
+                        content: "就想建个博客记录自己写bug的一系列过程(*ﾟДﾟ*) "
+                    }).then(function () {
+                        botui.action.button({
+                            delay: 1500,
+                            action: [{
+                                text: "你有女朋友吗？(･ェ･。)",
+                                value: "why-domain"
+                            }]
+                         }).then(function (a) {
+                        end()
                     })
                 })
             })
         },
-        fifthpart = function () {
-            botui.message.add({
-                delay: 1E3,
-                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
-            }).then(function () {
+            end = function () {
                 botui.message.add({
-                    delay: 1600,
-                    content: "那么，仔细看看我的博客吧？ ^_^"
+                    delay: 600,
+                    content: "![...](https://cdn.jsdelivr.net/gh/MashiroRb/CDNjsDelivr@v1.7/lpzn.jpg)"
                 })
             })
-        } 
+        }
 }
